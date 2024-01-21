9d9a1e811605194b29f5abec551ee0ee0e4cdfdb
Magomed-Darchi <gorchm93@gmail.com>
2024-01-21 21:04:45 +0300

    создание докер композе

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-print("Hello world")
+from tu import total_two_numbers
+print(total_two_numbers(9,6))
---
+++ tu.py
@@ -0,0 +1,10 @@
+
+def total_two_numbers(a,b):
+    return a+b
+
+
+# for i in range(10,101):
+#     if i%4==0 or i%6==0:
+#         print(total_two_numbers(i,i+1))
+
+
